1b323913ba32b83c5baa82ebe84815abb8c69e2d
ji-hokim <jimmy7335@korea.ac.kr>
2024-03-26 10:34:29 +0900

    solving 4

--- src/test/java/com/buildnow/springbootapp/buildnowspringboot/BuildnowSpringbootApplicationTests.java
+++ src/test/java/com/buildnow/springbootapp/buildnowspringboot/BuildnowSpringbootApplicationTests.java
@@ -3,7 +3,7 @@ package com.buildnow.springbootapp.buildnowspringboot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BuildnowSpringbootApplicationTests.class)
 class BuildnowSpringbootApplicationTests {
 
 	@Test
